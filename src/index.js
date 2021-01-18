c5c7f2ec60e243bf79169ecf8c88b20efc91f8e6
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2021-01-19 00:14:33 +0100

    feat: eslint and prettier formatter added more styles added to jumbotron as well

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import App from './app';
-import { GlobalStyles } from './global-styles'
+import { GlobalStyles } from './global-styles';
+import 'normalize.css';
 
 render(
   <>
-  <GlobalStyles />
-  <App />
-  </>,  
+    <GlobalStyles />
+    <App />
+  </>,
   document.getElementById('root')
 );
