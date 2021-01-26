bd1be176e860117a60fa9be34236626f6d9f4594
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2021-01-26 13:34:55 +0100

    feat: edge cases to redirect logged-in users logged-in users that go to the signin or signup routes are redirected to the browse page

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, Signin, Signup } from './pages';
+import { IsUserRedirect } from './helpers/routes';
 
 function App() {
+  const user = {};
+
   return (
     <Router>
       <Switch>
+        <IsUserRedirect exact path={ROUTES.SIGN_IN} loggedInPath={ROUTES.BROWSE} user={user}>
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect exact path={ROUTES.SIGN_UP} loggedInPath={ROUTES.BROWSE} user={user}>
+          <Signup />
+        </IsUserRedirect>
         <Route exact path={ROUTES.BROWSE}>
           <Browse />
-        </Route>
-        <Route exact path={ROUTES.SIGN_IN}>
-          <Signin />
-        </Route>
-        <Route exact path={ROUTES.SIGN_UP}>
-          <Signup />
         </Route>
         <Route exact path={ROUTES.HOME}>
           <Home />
---
+++ src/helpers/routes.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+export function IsUserRedirect({ user, loggedInPath, children, ...restProps }) {
+  return (
+    <Route
+      {...restProps}
+      render={() => {
+        if (!user) {
+          return children;
+        }
+
+        if (user) {
+          return (
+            <Redirect
+              to={{
+                pathname: loggedInPath,
+              }}
+            />
+          );
+        }
+        return null;
+      }}
+    />
+  );
+}
