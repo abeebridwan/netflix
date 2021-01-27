54ce76194747b0ccea7a36b3379ea16a37b29d68
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2021-01-27 17:34:00 +0100

    feat: filter added for films and series docs

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
   const { series } = useContent('series');
   const { films } = useContent('films');
-  console.log(series);
-  console.log(films);
+  const slides = selectionFilter({ series, films });
+  console.log(slides);
   return <p>Hello from the browse</p>;
 }
