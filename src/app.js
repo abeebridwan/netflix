8f65cf6db35775712f65a7b1673361047c91318e
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2021-01-27 16:04:15 +0100

    feat: auth-listener added auth-listener filled/remove user data from the pages and the  localstorage

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, Signin, Signup } from './pages';
 import { ProtectedRoute, IsUserRedirect } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 function App() {
-  const user = {};
+  const { user } = useAuthListener();
+  console.log(user);
 
   return (
     <Router>
